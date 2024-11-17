ff87aff335abe22e4f17a6c7cc17a4da53342107
trydemo <nickjagtap56@gmail.com>
2024-11-17 10:59:34 +0100

    Second_update

--- R_P_S_game.cpp
+++ R_P_S_game.cpp
@@ -1,1 +1,56 @@
 # include <iostream>
+
+char getUserChoice();
+char getComputerChoice();
+void showChoice(char choice);
+void chooseWinner (char player, char computer);
+
+int main ()
+{
+
+    char player;
+    char computer;
+
+    player = getUserChoice();
+
+
+}
+
+
+char getUserChoice()
+{   
+    char player;
+    std::cout << "Rock-Paper-Scissors Game. \n";
+    
+    do {
+        std::cout << "Choose one of the following\n";
+        std::cout << "************************* \n";
+        std::cout << "'r' for rock \n";
+        std::cout << "'p' for paper \n";
+        std::cout << "'s' for Scissors \n";
+        std::cin >> player;
+    }while (player != 'r' && player != 'p' && player != 's');
+    {
+        return player;
+
+    }
+    
+    
+    return 0;
+
+}
+
+char getComputerChoice()
+{   return 0;
+
+}
+
+void showChoice(char choice)
+{
+
+}
+
+void chooseWinner (char player, char computer)
+{
+
+}
